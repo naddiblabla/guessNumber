dfe24ca816d1d3deae0bf5ee63b297eb2cee91cd
Nadja <nadjabergqvist@gmail.com>
2019-03-27 20:45:19 +0100

    Deleted junk

--- guessNumber/guessNumber.cpp
+++ guessNumber/guessNumber.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include <string>
+//#include <string>
+#include <ctime>
 
 using namespace std;
 int main()
 {
-	int randomNumber = rand() % 10;  //random number 0-9
-	cout << "I've come up with a number 0-9. Your job is to guess which number it is. Good luck!\n";
-	int guessedNumber = 10;
+	srand(time(0));  // Initialize random number generator.
+	int randomNumber = (rand() % 10) + 1;  // Random number 1-10
+	cout << "I've come up with a number 1-10. Your job is to guess which number it is. Good luck!\n";
+	
+	int guessedNumber = 0;
 	while (guessedNumber != randomNumber)
 	{
-		cin >> guessedNumber;
-		if (guessedNumber != randomNumber) cout << "Try again.\n";
+		if (cin >> guessedNumber && guessedNumber > 0)
+		{
+			if (guessedNumber < randomNumber) cout << "Too low, try again.\n";
+			else if (guessedNumber > randomNumber) cout << "Too high, try again.\n";
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Try a number.\n";
+		}
 	}
 	cout << "You're right! The number is " << randomNumber << ".";
 }
